f72be1be21c468c465ff031dc97a100990702629
pinklightsaber <ctorres@idemar.cl>
2018-06-10 22:01:30 -0400

    agregando parametros y exclusiones, español y C

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ $(function() {
 	var coords = { //latitud, longitud
 		scl: '-33.4488897,-70.6692655',
 		ccp: '-36.8201352,-73.0443904'
-	}
+	};
+	var queryParams = ['exclude=[minutely,hourly,daily,alerts,flags]', 'lang=es', 'units=auto'];
+
+
 
 	$('#select').on('change', function(){
 		$.ajax ({
-		url: url + key + coords[$(this).val()],
+		url: url + key + coords[$(this).val()] + '?'+queryParams[0]+'&'+queryParams[1] + '&' + queryParams[2],
 		method: 'GET'
 		}).then(function(data){
 			console.log(data);
